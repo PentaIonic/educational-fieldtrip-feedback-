35cdb374fc0313a3329acd72f2b56a689721464e
PentaIonic <johnfernandojorvina@gmail.com>
2025-04-25 21:45:38 +0800

    Index Fix

--- scripts/tourismForm.js
+++ scripts/tourismForm.js
@@ -1,13 +1,13 @@
-const fieldtripSurvey = document.getElementById("fieldtripSurvey");
+const tourismSurvey = document.getElementById("tourismSurvey");
 const promptSuccess = document.getElementById("promptSuccess");
 const promptMessage = document.getElementById("promptMessage");
 
 //Listens to submit button within the website
-fieldtripSurvey.addEventListener("submit", function (e) {
+tourismSurvey.addEventListener("submit", function (e) {
   e.preventDefault();
 
   //Condition where if the user's input were missing or invalid, will be denied for submission.
-  if (!fieldtripSurvey.checkValidity()) {
+  if (!tourismSurvey.checkValidity()) {
     promptDenied.style.display = "flex";
     return;
   } else {
@@ -25,11 +25,11 @@ function displayPrompt() {
 //Function for storing the data in web's localStorage.
 function confirmSubmission() {
   //Instantiate a FormData library.
-  const formData = new FormData(fieldtripSurvey);
+  const formData = new FormData(tourismSurvey);
 
   //Adds every value from ticked checkboxes.
   const amenities = [];
-  fieldtripSurvey
+  tourismSurvey
     .querySelectorAll('input[name="amenities"]:checked')
     .forEach((checkbox) => {
       amenities.push(checkbox.value);
@@ -37,16 +37,6 @@ function confirmSubmission() {
   //Adds the alternate string instead of displaying null/blank
   if (amenities.length === 0) {
     amenities.push("Not Observed/Used");
-  }
-
-  const favorite = formData.get("favorite").trim();
-  if (!favorite) {
-    formData.set("favorite", "No Favorites");
-  }
-
-  const learnFeedback = formData.get("learned").trim();
-  if (!learnFeedback) {
-    formData.set("learned", "N/A");
   }
 
   const suggestions = formData.get("suggestions").trim();
@@ -60,9 +50,6 @@ function confirmSubmission() {
     gradeLevel: formData.get("grade"),
     destination: formData.get("destination"),
     visitDate: formData.get("visit-date"),
-    favorite: formData.get("favorite"),
-    learnFeedback: formData.get("learned"),
-    educRating: formData.get("educStars"),
     cleanRating: formData.get("cleanStars"),
     amenities: amenities,
     suggestions: formData.get("suggestions"),
@@ -74,5 +61,5 @@ function confirmSubmission() {
   localStorage.setItem("feedbackData", JSON.stringify(feedbackData));
 
   //Resets the fields in the form.
-  fieldtripSurvey.reset();
+  tourismSurvey.reset();
 }
