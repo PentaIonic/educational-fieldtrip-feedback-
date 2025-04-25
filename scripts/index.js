962c77176596b554eb3c7039a8e6f59bb7a607e9
PentaIonic <johnfernandojorvina@gmail.com>
2025-04-25 23:03:51 +0800

    Index Images fix?

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const switcheroo = document.getElementsByClassName("section-1")[0]; // assuming only one element
+    const switcheroo = document.getElementsByClassName("section-1")[0];
     const captionDisplay = document.getElementsByClassName("caption")[0];
 
     const backgrounds = [
-        "url(../../../assets/images/screensaver1.webp) no-repeat center center",
-        "url(../../../assets/images/screensaver2.webp) no-repeat center center",
-        "url(../../../assets/images/screensaver3.webp) no-repeat center center",
-        "url(../../../assets/images/screensaver4.webp) no-repeat center center"
+        "url(../assets/images/screensaver1.webp) no-repeat center center",
+        "url(../assets/images/screensaver2.webp) no-repeat center center",
+        "url(../assets/images/screensaver3.webp) no-repeat center center",
+        "url(../assets/images/screensaver4.webp) no-repeat center center"
     ];
 
     function delay(ms) {
