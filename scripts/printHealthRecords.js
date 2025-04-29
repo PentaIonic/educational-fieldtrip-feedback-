3fcaf09138b073fa0effcc655890eb0ebbe6dcbd
PentaIonic <johnfernandojorvina@gmail.com>
2025-04-29 13:13:26 +0800

    Refactor health and tourism records: update column names and enhance data handling in print views

--- scripts/printHealthRecords.js
+++ scripts/printHealthRecords.js
@@ -15,29 +15,3 @@ function printTable() {
     };
   };
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  const healthData = JSON.parse(localStorage.getItem("healthData")) || [];
-  const tableBody = document.querySelector("#healthDataResults tbody");
-
-  if (healthData.length === 0) {
-    const noDataRow = document.createElement("tr");
-    noDataRow.innerHTML = `<td colspan="9" style="text-align:center;" class="noDataRow">No Data Available</td>`;
-    tableBody.appendChild(noDataRow);
-  } else {
-    healthData.forEach((entry, index) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-    <td>${index + 1}</td>
-    <td>${entry.fullName}</td>
-    <td>${entry.age}</td>
-    <td>${entry.gender}</td>
-    <td>${entry.address}</td>
-    <td>${entry.vaccineStatus}</td>
-    <td>&bull; ${entry.symptom.join("<br> &bull; ")}</td>
-    <td>${entry.lastMedicalCheckupDate}</td>
-    <td>${entry.healthNotes}</td>`;
-      tableBody.appendChild(row);
-    });
-  }
-});
--- scripts/printTourismRecords.js
+++ scripts/printTourismRecords.js
@@ -15,28 +15,3 @@ function printTable() {
     };
   };
 }
-
-document.addEventListener("DOMContentLoaded", function () {
-  const feedbackData = JSON.parse(localStorage.getItem("feedbackData")) || [];
-  const tableBody = document.querySelector("#feedbackDataResults tbody");
-
-  if (feedbackData.length === 0) {
-    const noDataRow = document.createElement("tr");
-    noDataRow.innerHTML = `<td colspan="8" style="text-align:center;" class="noDataRow">No Data Available</td>`;
-    tableBody.appendChild(noDataRow);
-  } else {
-    feedbackData.forEach((entry, index) => {
-      const row = document.createElement("tr");
-      row.innerHTML = `
-  <td>${index + 1}</td>
-  <td>${entry.name}</td>
-  <td>${entry.nationality}</td>
-  <td>${entry.destination}</td>
-  <td>${entry.visitDate}</td>
-  <td style="text-align: center;">${entry.cleanRating}/5 &#9734;</td>
-  <td>&bull; ${entry.amenities.join("<br> &bull; ")}</td>
-  <td>${entry.suggestions}</td>`;
-      tableBody.appendChild(row);
-    });
-  }
-});
